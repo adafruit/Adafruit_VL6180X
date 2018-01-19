57dbf6b91e61ca65443f195cefa8ef4522c48a1b
Travis CI <travis@travis-ci.org>
2018-01-19 17:25:40 +0000

    Deploy code docs to GitHub Pages Travis build: 3
    Commit: 1d95cc41d5f8a1710ad1575eb769dbfaea2ae9ae

--- html/search/pages_0.js
+++ html/search/pages_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['adafruit_20vl6180x_20tof_20sensor_20driver',['Adafruit VL6180X ToF sensor driver',['../index.html',1,'']]],
-  ['adafruit_20vl6180x_20library_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fvl6180x_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fvl6180x_29',['Adafruit VL6180X Library [![Build Status](https://travis-ci.org/adafruit/Adafruit_VL6180X.svg?branch=master)](https://travis-ci.org/adafruit/Adafruit_VL6180X)',['../md__home_travis_build_adafruit__adafruit__v_l6180_x__r_e_a_d_m_e.html',1,'']]]
+  ['adafruit_20vl6180x_20library_20_5b_21_5bbuild_20status_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fvl6180x_2esvg_3fbranch_3dmaster_29_5d_28https_3a_2f_2ftravis_2dci_2eorg_2fadafruit_2fadafruit_5fvl6180x_29',['Adafruit VL6180X Library [![Build Status](https://travis-ci.org/adafruit/Adafruit_VL6180X.svg?branch=master)](https://travis-ci.org/adafruit/Adafruit_VL6180X)',['../md__r_e_a_d_m_e.html',1,'']]]
 ];

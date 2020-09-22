0370c5eb28a97a0a217c025a50344f4311a42cf9
Konstantin Grech <kgrech@mail.ru>
2020-09-22 22:48:34 +0300

    Custom init address

--- Adafruit_VL6180X.cpp
+++ Adafruit_VL6180X.cpp
@@ -39,7 +39,7 @@
     @brief  Instantiates a new VL6180X class
 */
 /**************************************************************************/
-Adafruit_VL6180X::Adafruit_VL6180X(void) {}
+Adafruit_VL6180X::Adafruit_VL6180X(uint8_t i2caddr) : _i2caddr(i2caddr) {}
 
 /**************************************************************************/
 /*!
@@ -50,7 +50,6 @@ Adafruit_VL6180X::Adafruit_VL6180X(void) {}
 */
 /**************************************************************************/
 boolean Adafruit_VL6180X::begin(TwoWire *theWire) {
-  _i2caddr = VL6180X_DEFAULT_I2C_ADDR;
   if (!theWire) {
     _i2c = &Wire;
   } else {
